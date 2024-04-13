5f9756e51df4fd697961e92a693f519b45000f95
damien droisy <damien.droisy@gmail.com>
2024-04-13 15:05:06 +0200

    création de la superclasse projectile

---
+++ core/src/com/mygdx/game/GameScreen/Entity/Characters/Projectile/Projectile.java
@@ -0,0 +1,27 @@
+package com.mygdx.game.GameScreen.Entity.Characters.Projectile;
+
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Vector2;
+import com.mygdx.game.GameScreen.Entity.Characters.Character;
+import com.mygdx.game.GameScreen.Entity.Entity;
+import com.mygdx.game.GameScreen.Worlds.World;
+
+public abstract class Projectile extends Character {
+
+    public Projectile(String name, Vector2 position, float speed, float health, World world) {
+        super(name, position, speed, health, world);
+    }
+
+    public abstract void attack();
+
+    @Override
+    public void update() {
+        for (Entity entity : currentWorld.entities)
+        {
+            if (Intersector.overlaps(entity.rect, rect))
+            {
+                attack();
+            }
+        }
+    }
+}
